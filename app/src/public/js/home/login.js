5955e7c30e7e20003cab0302f09b4ca7c70600ee
github wns0901 <github wnswns012@gmail.com>
2022-12-01 15:28:13 +0900

    로그인을 요총하는 fatch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,4 +13,12 @@ function login() {
     pw: pw.value,
   };
   console.log(req);
+  console.log(JSON.stringify(req));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
