36aaeff020bc53d2597d2a1fbe9e3341783e50a6
github wns0901 <github wnswns012@gmail.com>
2022-12-01 14:12:52 +0900

    js확인

---
+++ app/app.js
@@ -0,0 +1,15 @@
+//모듈
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//라우터
+const home = require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home);
+
+module.exports = app;
