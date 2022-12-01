0b8af62e864ad2b377b7436725216e01c9d3174e
github wns0901 <github wnswns012@gmail.com>
2022-12-01 14:54:28 +0900

    dom을 이용한 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("hello ");
-console.log("bye");
+const id = document.querySelector("#id"),
+  pw = document.querySelector("#pw"),
+  loginbtn = document.querySelector("button");
+
+console.log(id);
+
+loginbtn.addEventListener("click", login);
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+}
