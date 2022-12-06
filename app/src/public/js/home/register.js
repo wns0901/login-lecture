cb3035dd307ec7763f98b2ace44e394b446f98a2
wns0901 <wnswns012@gmail.com>
2022-12-06 21:01:53 +0900

    확인

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,12 +9,18 @@ const id = document.querySelector("#id"),
 registerbtn.addEventListener("click", register);
 console.log("헬로 레지스터");
 
+
 function register() {
+  if(!id.value) return alert("아이디를 입력해주십시오.")
+  if(pw.value !== confirmPw.value) {
+    return alert("비밀번호가 일치하지 않습니다.");
+  }
+
   const req = {
     id: id.value,
     pw: pw.value,
     name: name.value,
-    confirmPw: confirmPw.value,
+    
   };
 
   console.log(req);
