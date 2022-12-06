4dc9e6b75fb14f003d0721d9632934c038a152cc
github wns0901 <github wnswns012@gmail.com>
2022-12-06 14:30:35 +0900

    구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,5 +22,14 @@ function login() {
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
-    .then(console.log);
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인중 에러 발생"));
+    });
 }
